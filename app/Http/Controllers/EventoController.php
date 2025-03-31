430c3d8f9a76f5c46aa1f04eb92ff2e262eac714
Cleo079 <ivan.martinezllansana@gmail.com>
2025-03-31 15:47:18 +0200

    adicion base de datos
    adicion de vinculacion a base de datos desde archivo .env

--- app/Http/Controllers/EventoController.php
+++ app/Http/Controllers/EventoController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Evento;
 use Illuminate\Http\Request;
 
-class EventoController extends Controller
+class EventoController extends Evento
 {
     /**
      * Display a listing of the resource.
--- resources/views/feria.blade.php
+++ resources/views/feria.blade.php
@@ -7,26 +7,26 @@
     <div class="d-flex justify-content-center align-items-center vh-100">
         <div class="card p-4 shadow-lg" style="width: 350px;">
             <h3 class="text-center mb-4">Feria</h3>
-            <form>
+            <form action="" method="POST">
                 <div class="mb-3">
                     <label for="nombre" class="form-label">Nombre</label>
                     <input type="text" class="form-control" id="nombre" placeholder="Introdzca nombre de feria">
                 </div>
                 <div class="mb-3">
-                    <label for="textarea" class="form-label">Descripcion</label>
-                    <textarea class="form-control" id="textarea" rows="3" placeholder="Descripcion feria..."></textarea>
+                    <label for="descripcion" class="form-label">Descripcion</label>
+                    <textarea class="form-control" id="descripcion" rows="3" placeholder="Descripcion feria..."></textarea>
                 </div>
                 <div class="mb-3">
-                    <label for="fehcaFeria" class="form-label">Fecha</label>
-                    <input class="form-control" type="date" id="fehcaFeria">
+                    <label for="fecha" class="form-label">Fecha</label>
+                    <input class="form-control" type="date" id="fecha">
                 </div>
                 <div class="mb-3">
-                    <label for="tiempo" class="form-label">Horario</label>
-                    <input type="time" id="tiempo" class="form-control">
+                    <label for="hora" class="form-label">Horario</label>
+                    <input type="time" id="hora" class="form-control">
                 </div>
                 <div class="mb-3">
-                    <label for="lugart" class="form-label">Lugar</label>
-                    <input type="text" id="lugart" class="form-control">
+                    <label for="lugar" class="form-label">Lugar</label>
+                    <input type="text" id="lugar" class="form-control">
                 </div>
                 <div class="mb-3">
                     <label for="aforo" class="form-label">Aforo</label>
