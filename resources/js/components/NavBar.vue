939c3fb71e74c89a0dc097f5c708a62f88945049
Cleo079 <ivan.martinezllansana@gmail.com>
2025-03-31 18:01:23 +0200

    cambio nombre
    cambio nombre Dropdown

--- resources/js/components/NavBar.vue
+++ resources/js/components/NavBar.vue
@@ -23,7 +23,7 @@
                 <a class="nav-link" href="#">About</a>
                 </li>
                 <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Menu</a>
                 <div class="dropdown-menu">
                     <a class="dropdown-item" href="{{ url('usuario') }}">Usuarios</a>
                     <a class="dropdown-item" href="{{ url('evento') }}">Eventos</a>
