072709aa6874f98f4b1db522a63ae148f011ce23
Cleo079 <ivan.martinezllansana@gmail.com>
2025-05-16 18:09:55 +0200

    sss
    ssss

--- resources/js/components/Entradas.vue
+++ resources/js/components/Entradas.vue
@@ -3,56 +3,7 @@
 
       <div class="row">
         <p>Carrito({{ carrito }})</p>
-        <div
-          v-for="evento in eventos"
-          :key="evento.ID_EVENTO"
-        >
-          <div class="card h-100 shadow-sm">
-            <img
-              :src="imagen"
-              class="card-img-top"
-              alt="Imagen del evento"
-            />
-            <div class="card-body d-flex flex-column">
-              <h5 class="card-title">{{ titulo + ' ' + evento.NOMBRE }}</h5>
-              <p class="card-text">
-                <strong>Fecha:</strong> {{ evento.FECHA }}<br />
-                <strong>Lugar:</strong> {{ evento.LUGAR }}
-              </p>
-              <div v-for="(variante, indice) in variante"
-              :key="variante.id"
-              @mouseover="updateVariante(indice)"
-              class="circulo"
-              :style="{ backgroundImage: variante.imagen}"></div>
-              <p v-if="stock > 10" class="card-text">Stock</p>
-              <p v-else-if="stock <= 10 && stock > 0" class="card-text">Poco Stock</p>
-              <p v-else="stock" class="card-text">No Stock</p>
-              <button
-                class="btn btn-primary mt-auto"
-                @click="anadirCarro(), comprarEntrada(evento.ID_EVENTO)"
-                :disabled="!stock"
-              >
-                Comprar Entrada
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
 
-      <!-- Entrada generada -->
-      <div v-if="entrada" class="mt-5">
-        <div class="card border-success shadow-sm">
-          <div class="card-header bg-success text-white">
-            Entrada Generada
-          </div>
-          <div class="card-body">
-            <h5 class="card-title">{{ entrada.evento.NOMBRE }}</h5>
-            <p class="card-text">
-              <strong>Fecha de compra:</strong> {{ entrada.FECHA_ENTRADA }}<br />
-              <strong>Fecha del evento:</strong> {{ entrada.evento.FECHA }}
-            </p>
-          </div>
-        </div>
       </div>
 
     </div>
@@ -60,79 +11,18 @@
 
   <script>
   export default {
-    data() {
+    data() {//eliminar todo menos carrito
       return {
-        carrito: 0,
-        marca: 'Entradas para',
-        varianteSeleccionada: 0,
-        eventos: [],
-        entrada: null,
-        idUsuario: 1, // Simulado, cámbialo cuando tengas auth
-        variante: [
-            {id: 1024, color: 'darkgreen', imagen: '/imgs/IS-2.jpg', cantidad: 50},
-            {id: 1025, color: 'brown', imagen: '/imgs/evento_special.jpg', cantidad: 0},
-        ]
+        carrito: 0
       };
     },
-    mounted() {
-        axios
-        .get('evento')
-        .then(response => {
-            console.log(response)
-            this.eventos = response.data
-        })
+    methods: {//dejar el apartado methods pero vacia
+
     },
-    // mounted() {
-    //   fetch("http://localhost/Intermodular-JI/public/api/evento")
-    //     .then((res) => res.json())
-    //     .then((data) => (this.eventos = data));
-    // },
-    methods: {
-      comprarEntrada(idEvento) {
-        fetch("http://localhost/Intermodular-JI/public/api/entradas", {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({
-            ID_USUARIO: this.idUsuario,
-            ID_EVENTO: idEvento,
-          }),
-        })
-          .then((res) => res.json())
-          .then((data) => {
-            this.entrada = data.entrada;
-        });
-      },
-      anadirCarro() {
-        this.carrito += 1
-      },
-      updateVariante(indice) {
-        this.varianteSeleccionada = indice
-      }
-    },
-    computed: {
-        titulo() {
-            return this.marca
-        },
-        imagen() {
-            return this.variante[this.varianteSeleccionada].imagen
-        },
-        stock() {
-            return this.variante[this.varianteSeleccionada].cantidad
-        }
-    }
   };
   </script>
 
   <style scoped>
-  .card-img-top {
-    object-fit: cover;
-    height: 150px;
-  }
-  .circulo {
-    width: 50px;
-    height: 50px;
-    margin-top: 8px;
-    border: 2px solid #d8d8d8;
-    border-radius: 50%;
-  }
+
+
   </style>
