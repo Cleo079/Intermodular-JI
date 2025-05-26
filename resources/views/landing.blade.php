171720d2a6ea49c6e8afcab1a56a1a52f80677a8
Cleo079 <ivan.martinezllansana@gmail.com>
2025-05-26 10:14:04 +0200

    sss
    sssss

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -22,36 +22,7 @@
     gran variedad de ferias que seguro te interesaran
 </p>
 <div class="contenedor" id="app">
-    <div id="registro"><registro></registro></div>
-    <div id="login">
-        <div class="d-flex justify-content-center align-items-center vh-100">
-            <div class="card p-4 shadow-lg" style="width: 350px;">
-                <h3 class="text-center mb-4 darkGreen">Login</h3>
-                <form method="post" action="{{ route('iniciar') }}">
-                    @csrf
-
-                    <div class="mb-3">
-                        <label for="email" class="form-label green">Email</label>
-                        <input type="email" class="form-control" id="email" name="email" placeholder="Introduce tu email">
-                    </div>
-                    <div class="mb-3">
-                        <label for="password" class="form-label green">Contraseña</label>
-                        <input type="password" class="form-control" id="password" name="password" placeholder="Introduce tu contraseña">
-                    </div>
-                    <div class="mb-3">
-                        <p>Aun no tienes cuenta? <a href="{{ route("registro") }}">Registrate aqui</a></p>
-                    </div>
-                    <div class="d-flex justify-content-center">
-                        <button type="submit" class="btn btn-primary">Iniciar sesión</button>
-                    </div>
-
-                    @if(session('error'))
-                        <p class="text-danger text-center mt-3">{{ session('error') }}</p>
-                    @endif
-                </form>
-            </div>
-        </div>
-        </div>
+    <mi-elemento></mi-elemento>
 
 </div>
 
@@ -59,4 +30,4 @@
 
 @endsection
 
-
+<script src="../js/web-components.js"></script>
